c1d18ccd20b7fb331f5bebf63d6fa14748156e70
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-07-28 15:07:55 +0300

    did task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+// console.log(refs.gallery.text);
+
+const galleryItemsDivs = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </li>`;
+  })
+  .join("");
+
+refs.gallery.innerHTML = galleryItemsDivs;
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
