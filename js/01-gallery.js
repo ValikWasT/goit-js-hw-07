7680937b0ef9c11b91307aa2fc0a09991360b12f
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-07-29 11:22:09 +0300

    fix onEscapeClose

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -50,7 +50,12 @@ refs.gallery.innerHTML = galleryItemsDivs;
 refs.gallery.addEventListener("click", (evt) => {
   evt.preventDefault();
   // console.log(evt.target.classList.contains("gallery__image"));
-
+  const onEsapeClose = ({ code }) => {
+    console.log(code);
+    if (code === "Escape") {
+      instance.close();
+    }
+  };
   if (!evt.target.classList.contains("gallery__image")) {
     return;
   }
@@ -62,17 +67,31 @@ refs.gallery.addEventListener("click", (evt) => {
             data-source="${evt.target.dataset.source}"
             alt="${evt.target.alt}"
             />
-	`
+	`,
+    {
+      /*
+       * Prevents the lightbox from closing when clicking its background.
+       */
+      closable: true,
+      /*
+       * One or more space separated classes to be added to the basicLightbox element.
+       */
+      className: "",
+      /*
+       * Function that gets executed before the lightbox will be shown.
+       * Returning false will prevent the lightbox from showing.
+       */
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEsapeClose);
+      },
+      /*
+       * Function that gets executed before the lightbox closes.
+       * Returning false will prevent the lightbox from closing.
+       */
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEsapeClose);
+      },
+    }
   );
   instance.show();
-  const onEsapeClose = ({ code }) => {
-    console.log(code);
-    if (code === "Escape") {
-      instance.close();
-      window.removeEventListener("keydown", onEsapeClose);
-    }
-  };
-  if (instance.show()) {
-    window.addEventListener("keydown", onEsapeClose);
-  }
 });
